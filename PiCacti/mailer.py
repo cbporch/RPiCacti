97cf3d55d41452699372b09272833353dcefa4e8
Christopher Porch <cbporch@gmail.com>
2017-06-05 16:59:59 -0400

    reworking/adding to mailer

--- PiCacti/mailer.py
+++ PiCacti/mailer.py
@@ -1,7 +1,15 @@
 import smtplib
+import json
 
 from email.mime.text import MIMEText
 
 
-def send_mail():
-    pass
+class Mailer:
+    def __init__(self):
+        # read parameters from JSON file
+        with open('secrets.json', 'r') as fp:
+            secrets_json = json.load(fp)
+        self.SENDER = secrets_json["sender-email"]
+
+    def send_mail(self):
+        pass
