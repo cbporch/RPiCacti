174ccb0e0c359069ae63987ec79a9666e0e0b1e8
Christopher Porch <cbporch@gmail.com>
2017-06-05 17:32:49 -0400

    changes to mailer

--- PiCacti/mailer.py
+++ PiCacti/mailer.py
@@ -10,6 +10,43 @@ class Mailer:
         with open('secrets.json', 'r') as fp:
             secrets_json = json.load(fp)
         self.SENDER = secrets_json["sender-email"]
+        self.RECEIVER = secrets_json["rec-email"]
 
-    def send_mail(self):
-        pass
+    def send_mail(self, inside=True):
+        if inside:
+            # bring inside message
+            with open("messages/inside.txt") as file:
+                msg = file.read()
+        else:
+            # send outside
+            with open("messages/outside.txt") as file:
+                msg = file.read()
+
+        msg = """From: Raspberry Pi <{0}>
+        To: Your Name <{1}>
+        Subject: PiCacti
+        
+        {2}
+        """.format(self.SENDER, self.RECEIVER, msg)
+
+        try:
+            s = smtplib.SMTP('localhost')
+            s.sendmail(self.SENDER, self.RECEIVER, msg)
+        except smtplib.SMTPException:
+            # something broke
+            print("unable to send mail")
+
+    def mail_test(self):
+        msg = """From: Raspberry Pi <{0}>
+                To: Your Name <{1}>
+                Subject: PiCacti
+
+                Test
+                """.format(self.SENDER, self.RECEIVER)
+
+        try:
+            s = smtplib.SMTP('localhost')
+            s.sendmail(self.SENDER, self.RECEIVER, msg)
+        except smtplib.SMTPException:
+            # something broke
+            print("unable to send mail")
